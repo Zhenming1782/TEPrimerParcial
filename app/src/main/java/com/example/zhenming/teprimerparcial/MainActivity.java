b2240f3a6559572889599782cb3496f6c573dcdb
Zhenming <zhenming@hotmail.es>
2019-10-08 23:56:52 -0400

    wawawawawawawawa

--- app/src/main/java/com/example/zhenming/teprimerparcial/MainActivity.java
+++ app/src/main/java/com/example/zhenming/teprimerparcial/MainActivity.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
                         et2.setText("");
                     }
                 }
-
 
             }
         });
